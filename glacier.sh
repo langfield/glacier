d6784866179045b8449391ffc21c7d3bc47a7ac4
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-02-17 20:22:18 +0000

    Vastly simplify everything

--- glacier.sh
+++ glacier.sh
@@ -10,11 +10,8 @@
 # Path to root password file
 rootpass_path="/usr/local/etc/glacier_rootpass"
 
-# Files we allow to be edited with this script
-valid_targets=()
-
 # Delay in seconds
-delay=7200
+delay=5
 
 ########################################################################
 # Here we go
@@ -22,21 +19,6 @@ delay=7200
 #####################
 # vvv FUNCTIONS vvv #
 #####################
-
-# Function to check if a variable is contained in an array
-contains() {
-    typeset _x;
-    typeset -n _A="$1"
-    for _x in "${_A[@]}" ; do
-            [ "$_x" = "$2" ] && return 0
-    done
-    return 1
-}
-
-# Function to print cmdline usage string
-print_usage() {
-  printf "Usage: glacier -r (displays rootpass) | glacier <path> (opens frozen file for reading) | glacier -w <path> (opens frozen file for writing)\n"
-}
 
 # Function to sleep while printing time remaining
 time_delay() {
@@ -48,13 +30,14 @@ time_delay() {
     done
     printf '\n'
 }
+# Function to print cmdline usage string
+print_usage() {
+  printf "Usage: glacier (displays rootpass) | glacier -a <URL> (append URL to /etc/hosts)\n"
+}
 
 #####################
 # ^^^ FUNCTIONS ^^^ #
 #####################
-
-# This variable expansion gets the basename of the path to this file
-progName="${0##*/}"
 
 # Reset PATH to known quantity
 export PATH=/usr/local/bin:/bin:/usr/bin
@@ -64,131 +47,36 @@ export PATH=/usr/local/bin:/bin:/usr/bin
 
 ########### ALL LOGIC SHOULD BE BELOW THIS POINT ###########
 
-# Path to valid targets file
 home=$(eval echo ~$SUDO_USER)
-valid_targets_path="$home/.glacier_targets"
-printf "Valid targets path: $valid_targets_path\n"
-if [ ! -f $valid_targets_path ]; then
-    echo "No targets file found at '$valid_targets_path'"
-    exit 1
-fi
 
-# Read valid targets from file
-IFS=$'\n' read -d '' -r -a valid_targets < $valid_targets_path
-valid_targets+=("$rootpass_path")
-valid_targets+=("$valid_targets_path")
-
-# Make sure the valid_targets are files that actually exist
-for valid_target in "${valid_targets[@]}"
-do
-    if [ ! -f $valid_target ]; then
-        echo "Target not found: $valid_target"
-        echo "You can add this file as a valid target by editing $valid_targets_path"
-        exit 1
-    fi
-done
-
-# Flag to indicate root password query
-r_flag=''
-
-# Flag to indicate we want to edit a file
-w_flag=''
-
+a_flag=''
 
 # Parse flags
-while getopts 'rw:' flag; do
+while getopts 'a:' flag; do
   case "${flag}" in
-    r) r_flag='true' ;;
-    w) w_flag='true' ;;
+    a) a_flag='true' ;;
     *) print_usage
-       printf "getopts is angry\n"
+       printf "bad cmdline flag\n"
        exit 1 ;;
   esac
 done
 
+# Append a line to the hosts file.
+if [[ "$a_flag" == "true" ]] ; then
+  printf "appending URL: $2\n"
+  echo "127.0.0.1 $2" >> /etc/hosts
+  tail /etc/hosts
+  exit 0
+fi
+
 # Handle root password query and then exit
-if [[ "$r_flag" == "true" ]] ; then
-    printf "Expected rootpass path: $rootpass_path\n"
-    if [ ! -f $rootpass_path ]; then
-        echo "No '$rootpass_path' file found."
-        echo "Run 'make rootpass' to generate this file."
-        exit 1
-    fi
-    time_delay
-    printf "ROOT PASSWORD:\n"
-    cat $rootpass_path
-    exit 0
-fi
-
-# Make sure we have at least one argument
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    print_usage
-    exit 0
-fi
-
-# Get abspath of the target
-if [[ "$w_flag" == "true" ]] ; then
-    target=`realpath $2`
-else
-    target=`realpath $1`
-fi
-
-# Check if the argument is a valid target
-if contains valid_targets "$target" ; then
-    echo "Good target: $target"
-else
-    echo "Bad target: $target (valid targets: ${valid_targets[@]})"
+printf "Expected rootpass path: $rootpass_path\n"
+if [ ! -f $rootpass_path ]; then
+    echo "No '$rootpass_path' file found."
+    echo "Run 'make rootpass' to generate this file."
     exit 1
 fi
-
-# Handle read query and then exit
-if [[ "$w_flag" == "" ]] ; then
-    time_delay
-    cat $target
-    exit 0
-fi
-
-umask 0022
-tmpf=$(mktemp "/tmp/${progName}_XXXXXXXXXX")
-cp -f "$target" "$tmpf"
-
-# Revert to the original user account to edit the temporary file
-chown "$SUDO_USER" "$tmpf"
-sudo -u "$SUDO_USER" "${EDITOR:-vi}" "$tmpf"
-
-
-printf "############################### OLD VERS: '%s' ###############################\n\n" $target
-cat $target
-printf "\n############################### NEW VERS: '%s' ###############################\n\n" $target
-cat $tmpf
-printf "\n############################### EDITS TO: '%s' ###############################\n\n" $target
-diff --new-file $target $tmpf
-printf "\n############################### --------: '%s' ###############################\n\n" $target
-
-# Make the user wait
 time_delay
-
-read -t 60 -p "Are you sure you still want to edit '$target' (y/N)? " YN
-if [[ ! "${YN,,}" =~ ^(y|yes)$ ]] ; then
-    printf "Aborting\n"
-    exit 1
-fi
-
-# If the copy of the file we edited ($tmpf) exists and we made nontrivial changes, apply them
-if [[ -f "$tmpf" ]] && ! cmp -s "$target" "$tmpf" ; then
-    echo 'Applying changes'
-
-    # Backup the target and then replace it with our edited version
-    [ ! -f "$target" ] && printf "Not making backup. Target '$target' does not exist\n"
-    [ -f "$target" ] && cp -pf "$target" "$target.old"
-    cp -f "$tmpf" "$target"
-else
-    printf "No changes\n"
-fi
-
-# All done
-echo 'Done'
-rm -f "$tmpf"
+printf "ROOT PASSWORD:\n"
+cat $rootpass_path
 exit 0
